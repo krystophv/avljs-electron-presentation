b5ce0db6f5f164ec150db3522d69546490221d02
Chris McGee <krystophv@gmail.com>
2024-01-07 14:26:23 -0500

    add github publish workflow

--- forge.config.ts
+++ forge.config.ts
@@ -9,6 +9,18 @@ const config: ForgeConfig = {
   packagerConfig: {},
   rebuildConfig: {},
   makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'krystophv',
+          name: 'avljs-electron-presentation',
+        },
+        draft: true,  
+      },
+    },
+  ],
   plugins: [
     new VitePlugin({
       // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
